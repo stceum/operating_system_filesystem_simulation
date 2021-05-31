c346ef1c9a4fb54a0c582459628b7be7a4fb407f
Scpjoker <2719323509@qq.com>
2021-05-31 19:58:19 +0800

    test

--- disk.cpp
+++ disk.cpp
@@ -1,1 +1,10 @@
 #include "disk.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int create_disk(char* disk_name, uint32_t disk_volumn)
+{
+    
+}
+
+
--- disk.h
+++ disk.h
@@ -5,6 +5,12 @@
 #define MAX_DISK_NAME_LEN 16 // 最长磁盘名长度(模拟磁盘的文件名长度)
 #define MAX_SECTOR_NAME_LEN 8 //最长分区名
 #define MAX_PARTIOTION_NUMBER 4 //最大分区数
+typedef struct partition
+{
+    uint32_t start_sector_no;            // 起始扇区号
+    uint32_t sector_count_per_partition; // 扇区长度
+    char partition_name[MAX_SECTOR_NAME_LEN];
+}partition;
 
 struct disk
 {
@@ -14,12 +20,7 @@ struct disk
     partition* partitions[MAX_PARTIOTION_NUMBER];
 };
 
-struct partition
-{
-    uint32_t start_sector_no;            // 起始扇区号
-    uint32_t sector_count_per_partition; // 扇区长度
-    char partition_name[MAX_SECTOR_NAME_LEN];
-};
+
 
 /*
  * 创建磁盘的函数
