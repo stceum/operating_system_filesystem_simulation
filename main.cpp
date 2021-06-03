e6370bb2d046a65d06fae37ed5cc6092f4fc19c8
stceum <zh_elepika@126.com>
2021-06-03 11:51:15 +0800

    update

--- main.cpp
+++ main.cpp
@@ -13,14 +13,14 @@ int main() {
   fs_init(&test_d, 0);
   //   uint32_t fd = sys_open("/file1", O_CREAT);
   uint32_t fd = sys_open("/file1", O_RDWR);
-//   write
-    sys_write(fd, "Hello world\n", 12);
-    sys_close(fd);
-    std::cout << fd << " closed!" << std::endl;
+  //   write
+  sys_write(fd, "Hello world\n", 12);
+  sys_close(fd);
+  std::cout << fd << " closed!" << std::endl;
   // read
-//   char test[64];
-//   memset(test, 0, 64);
-//   sys_read(fd, test, 64);
-//   std::cout << test << std::endl;
+  //   char test[64];
+  //   memset(test, 0, 64);
+  //   sys_read(fd, test, 64);
+  //   std::cout << test << std::endl;
   return 0;
 }
